91f42c627ebbce0c9f3ffd3f792608046f686b31
Alex Baranosky <alexander.baranosky@gmail.com>
2013-12-29 17:36:23 -0800

    need interactive to expose fn outside of its .el file

--- modules/alexb-erc.el
+++ modules/alexb-erc.el
@@ -5,9 +5,12 @@
       '(("freenode.net" "#clojure" "#emacs" "#bitcoin")))
 
 (defun alexb-start-erc ()
+  (interactive)
   ;; ;; TODO: make alexbaranosky
   (when (y-or-n-p "Do you want to start ERC? ")
     (erc :server "irc.freenode.net" :port 6667 :nick "abaranosky")))
 
 (alexb-start-erc)
+
+(provide 'alexb-erc)
 
