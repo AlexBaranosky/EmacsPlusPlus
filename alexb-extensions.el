31528a15310488ce15efe11cdf9b22f92bc990fc
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-03 15:29:48 -0800

    rename module -> extension AND made guru-mode only apply to prog-mode

---
+++ alexb-extensions.el
@@ -0,0 +1,11 @@
+(defun load-extension (filename)
+  (load (expand-file-name filename "~/.emacs.d/extensions")))
+
+(load-extension "alexb-cider-mode.el")
+(load-extension "alexb-clojure.el")
+(load-extension "alexb-erc.el")
+(load-extension "alexb-magit.el")
+(load-extension "alexb-paredit.el")
+
+(load-extension "alexb-catchall.el")
+
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
 (load (expand-file-name "~/.emacs.d/alexb-core.el"))
 (load (expand-file-name "~/.emacs.d/alexb-packages.el"))
-(load (expand-file-name "~/.emacs.d/alexb-modules.el"))
+(load (expand-file-name "~/.emacs.d/alexb-extensions.el"))
 
