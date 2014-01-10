9b6b19b0212e5896cc4e95e77771b58ee251d747
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-09 23:59:00 -0800

    Fixed file extension to .el

--- packages.el
+++ packages.el
@@ -1,8 +1,7 @@
 (when window-system
-  (require 'package))
-
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/") t))
 
 (add-to-list 'load-path "~/.emacs.d")
 (package-initialize)
