e963134677a5808f73756fd98b07a4e736122cc1
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-12 15:53:32 -0800

    uncomment newly-fixed ibuffer-modification code

--- core.el
+++ core.el
@@ -198,23 +198,23 @@ buffer is not visiting a file."
                      ""]))))
 
 ;; ;; Use human readable Size column instead of original one
-;; (define-ibuffer-column size-h
-;;   (:name "Size" :inline t)
-;;   (cond
-;;    ((> (buffer-size) 1000000) (format "%7.3fM" (/ (buffer-size) 1000000.0)))
-;;    ((> (buffer-size) 1000) (format "%7.3fk" (/ (buffer-size) 1000.0)))
-;;    (t (format "%8d" (buffer-size)))))
+(define-ibuffer-column size-h
+  (:name "Size")
+  (cond
+   ((> (buffer-size) 1000000) (format "%7.3fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 1000) (format "%7.3fk" (/ (buffer-size) 1000.0)))
+   (t (format "%8d" (buffer-size)))))
 
 ;; ;; Modify the default ibuffer-formats
-;; (setq ibuffer-formats
-;;       '((mark modified read-only " "
-;;               (name 18 18 :left :elide)
-;;               " "
-;;               (size-h 9 -1 :right)
-;;               " "
-;;               (mode 16 16 :left :elide)
-;;               " "
-;;               filename-and-process)))
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
 
 (defadvice ibuffer-update-title-and-summary (after remove-column-titles)
    (save-excursion
