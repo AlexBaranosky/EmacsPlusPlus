8c030a8b49e05c2fec4850cf9926161ed7440598
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-14 09:51:27 -0800

    make two thread-all macros and bind em as functions

--- extensions/alexb-clojure.el
+++ extensions/alexb-clojure.el
@@ -4,9 +4,20 @@
                                (clj-refactor-mode 1)
                                (cljr-add-keybindings-with-prefix "C-c C-t")))
 
+(fset 'cljr-thread-first-all
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([40 45 62 67108905 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926] 0 "%d")) arg)))
+
+
+(fset 'cljr-thread-last-all
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([40 67108905 45 62 62 32 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926 67108926] 0 "%d")) arg)))
+
 (define-key clojure-mode-map (kbd "C-x C-r") 'cljr-rename-file)
 (define-key clojure-mode-map (kbd "C->") 'cljr-thread)
 (define-key clojure-mode-map (kbd "C-<") 'cljr-unwind)
+
+(define-key clojure-mode-map (kbd "M-C->") 'cljr-thread-first-all)
+(define-key clojure-mode-map (kbd "M-C-?") 'cljr-thread-last-all)
+
 
 (defun live-delete-and-extract-sexp ()
   "Delete the sexp and return it."
