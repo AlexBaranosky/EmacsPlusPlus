e8639bfa0981a44173a04fdf46006b9a1140eccc
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-28 21:50:40 -0800

    add furtive dev when cleaning up clj files

--- extensions/alexb-projectile.el
+++ extensions/alexb-projectile.el
@@ -17,14 +17,25 @@
   (dolist (filename (projectile-current-project-file-full-paths))
     (cleanup-file filename)))
 
+(defun cljr-add-furtive-dev ()
+  (interactive)
+  (setq cljr-auto-sort-ns nil)
+  (ignore-errors
+    (cljr-add-require-to-ns)
+    (insert "[furtive.dev :refer :all]")
+    (yas-exit-all-snippets)
+    (cljr-sort-ns))
+  (setq cljr-auto-sort-ns t))
+
 (defun projectile-cleanup-project-clj-files ()
   (interactive)
   (dolist (filename (projectile-current-project-file-full-paths))
-    (when (and (s-ends-with? "clj" filename)
-               (not (s-ends-with? "project.clj" filename)))
+    (when (s-ends-with? "clj" filename)
       (find-file filename)
+      (cljr-add-furtive-dev)
       (cljr-sort-ns)
       (cleanup-file filename))))
+
 
 (global-set-key (kbd "C-c N") 'projectile-cleanup-project-buffers)
 (global-set-key (kbd "C-x M-f") 'projectile-find-file)
